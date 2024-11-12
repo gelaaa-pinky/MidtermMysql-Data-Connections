382d4f650544216d0da9ae47d049792b31ecef62
gelaaa-pinky <galangangelanicole917@gmail.com>
2024-11-12 10:54:05 +0700

    Add files via upload

---
+++ MYSQLDBConnection2022/Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        tbnTest = New Button()
+        SuspendLayout()
+        ' 
+        ' tbnTest
+        ' 
+        tbnTest.Location = New Point(242, 182)
+        tbnTest.Margin = New Padding(5)
+        tbnTest.Name = "tbnTest"
+        tbnTest.Size = New Size(204, 58)
+        tbnTest.TabIndex = 0
+        tbnTest.Text = "MYSQL TEST"
+        tbnTest.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(12F, 24F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1371, 720)
+        Controls.Add(tbnTest)
+        Font = New Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(5)
+        Name = "Form1"
+        Text = "MYSQLDB DATA CONNECTION"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents tbnTest As Button
+
+End Class
---
+++ MYSQLDBConnection2022/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+
+    Private Sub tbnTest_Click(sender As Object, e As EventArgs) Handles tbnTest.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.1;userid=root;password='';database=test"
+
+        Try
+            conn.Open()
+            MessageBox.Show("Connection to MySQL test database was successful!!!!", "TESTING      CONNECTION TO MySQL DATABASE")
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+End Class
